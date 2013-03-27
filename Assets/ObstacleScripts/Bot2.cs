3e7b522caf0a1d5ed85940357c914e1132fb5824
Jeremy Trunk <trunk.jeremy@gmail.com>
2013-03-27 15:59:17 -0700

    Added Pit.cs

--- Assets/ObstacleScripts/Bot2.cs
+++ Assets/ObstacleScripts/Bot2.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Bot2 : Player, Obstacle
 {
+	private bool hovering = false;
+		
 	public Bot2 () : base(2)
 	{
 	}
@@ -11,9 +13,21 @@ public class Bot2 : Player, Obstacle
 	{
 	}
 	
-	public override void primary(Tile a)
+	public void primary(Tile a, Tile b)
 	{
-		if (a.GetType () == typeof(Generator))
-			a.interact (this);
+		if (level >= 2) {
+			if (b != null) {
+				
+			}
+		}
+	}
+	
+	public override bool inAction()
+	{
+		//Hover Conditions
+		if (!hovering /*&&  Things*/){
+			return false;
+		} else 
+			return true;
 	}
 }
