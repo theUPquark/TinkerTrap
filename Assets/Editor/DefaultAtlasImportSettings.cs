4ca7225897658eb4895c0a4f25193630ea13159c
Silarn <jrim@rimpo.org>
2013-04-04 13:00:19 -0400

    Bot2 Moves Implemented
    Also tweaked importer code

--- Assets/Editor/DefaultAtlasImportSettings.cs
+++ Assets/Editor/DefaultAtlasImportSettings.cs
@@ -4,12 +4,22 @@ using UnityEditor;
 public class TexturePostProcessor : AssetPostprocessor {
  
 	void OnPostprocessTexture (Texture2D texture) {
-		TextureImporter importer = (TextureImporter)assetImporter;
+		TextureImporter importer = assetImporter as TextureImporter;
 		if (importer.assetPath.Contains ("Assets/SpriteAtlases")) {
 			importer.anisoLevel = 0;
 			importer.filterMode = FilterMode.Bilinear;
 			importer.maxTextureSize = 4096;
 			importer.textureFormat = TextureImporterFormat.AutomaticCompressed;
+			
+			Object asset = AssetDatabase.LoadAssetAtPath(importer.assetPath, typeof(Texture2D));
+			if (asset) {
+				EditorUtility.SetDirty(asset);
+			} else {
+				texture.anisoLevel = 0;
+				texture.filterMode = FilterMode.Bilinear;
+				texture.maxTextureSize = 4096;
+				texture.textureFormat = TextureImporterFormat.AutomaticCompressed;
+			}
 		}
 	}
  
